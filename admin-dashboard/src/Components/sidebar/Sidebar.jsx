1c8b345415341bcebc3b60d7c21e039653dc9b1e
kibria <shan2neo@gmail.com>
2022-06-18 23:55:36 +0600

    sidebar set up

--- admin-dashboard/src/Components/sidebar/Sidebar.jsx
+++ admin-dashboard/src/Components/sidebar/Sidebar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import "./sidebar.scss";
+import Aside from "./Aside";
+import { RiArrowLeftSLine } from "react-icons/ri";
+
 function Sidebar() {
-  return <div>Sidebar</div>;
+  const [collapsed, setCollapsed] = useState(false);
+  const handleCollapsedChange = (checked) => {
+    setCollapsed(checked);
+  };
+  return (
+    <div className="sidebar-container">
+      <Aside collapsed={collapsed} />
+      <div className="collapse-container"></div>
+      <RiArrowLeftSLine onClick={() => handleCollapsedChange(!collapsed)} />
+    </div>
+  );
 }
 
 export default Sidebar;
